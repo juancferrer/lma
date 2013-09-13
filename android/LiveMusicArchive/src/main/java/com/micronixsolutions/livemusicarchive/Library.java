a7e9092dfb8dad3ef8de07944e740c72079365dd
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2013-09-13 01:43:08 -0500

    Revert "Set the slidingmenu touch mode to 'margin'"
    This reverts commit 2e498fee29ec47d6c78d966484d7c3cffea27265.

--- android/LiveMusicArchive/src/main/java/com/micronixsolutions/livemusicarchive/Library.java
+++ android/LiveMusicArchive/src/main/java/com/micronixsolutions/livemusicarchive/Library.java
@@ -78,16 +78,18 @@ public class Library extends FragmentActivity implements ActionBar.TabListener {
         }
 
 
-        // Configure the SlidingMenu
+        // configure the SlidingMenu
         SlidingMenu menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         menu.setShadowWidthRes(R.dimen.shadow_width);
         menu.setShadowDrawable(R.drawable.shadow);
         menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
         menu.setFadeDegree(0.35f);
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         menu.setMenu(R.layout.sliding_menu);
+        menu.setContent(mViewPager);
+
     }
 
     @Override
