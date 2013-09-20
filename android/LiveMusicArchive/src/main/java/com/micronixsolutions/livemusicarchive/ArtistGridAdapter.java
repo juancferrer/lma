0dc2284d85e02980c40509092145e807e48f2f59
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2013-09-19 23:20:03 -0500

    Sorta uses endpoints to display data in gridview

--- android/LiveMusicArchive/src/main/java/com/micronixsolutions/livemusicarchive/ArtistGridAdapter.java
+++ android/LiveMusicArchive/src/main/java/com/micronixsolutions/livemusicarchive/ArtistGridAdapter.java
@@ -1,21 +1,43 @@
 package com.micronixsolutions.livemusicarchive;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.gson.GsonFactory;
+import com.micronixsolutions.api.music.Music;
+import com.micronixsolutions.api.music.model.MessagesArtistResponse;
+import com.micronixsolutions.api.music.model.MessagesArtistsResponse;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by juan on 9/18/13.
  */
-public class ArtistGridAdapter extends BaseAdapter {
+public class ArtistGridAdapter extends ArrayAdapter<MessagesArtistResponse> {
     private Context mContext;
 
     public ArtistGridAdapter(Context context){
+        super(context, R.layout.artist_grid_item, R.id.artist_grid_item_text);
         mContext = context;
     }
 
+    public void setData (List<MessagesArtistResponse> data){
+        clear();
+        if(data != null){
+            addAll(data);
+        }
+    }
+
+    /*
     @Override
     public int getCount() {
         return 100;
@@ -30,6 +52,7 @@ public class ArtistGridAdapter extends BaseAdapter {
     public long getItemId(int position) {
         return 0;
     }
+    */
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -38,6 +61,9 @@ public class ArtistGridAdapter extends BaseAdapter {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             gridItem = inflater.inflate(R.layout.artist_grid_item, parent, false);
         }
+        MessagesArtistResponse artist = getItem(position);
+        TextView tv = (TextView) gridItem.findViewById(R.id.artist_grid_item_text);
+        tv.setText(artist.getName());
         return gridItem;
     }
 }
--- android/LiveMusicArchive/src/main/java/com/micronixsolutions/livemusicarchive/ArtistsGridFragment.java
+++ android/LiveMusicArchive/src/main/java/com/micronixsolutions/livemusicarchive/ArtistsGridFragment.java
@@ -1,36 +1,132 @@
 package com.micronixsolutions.livemusicarchive;
 
 import android.content.Context;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ListAdapter;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.gson.GsonFactory;
+import com.micronixsolutions.api.music.Music;
+import com.micronixsolutions.api.music.model.MessagesArtistResponse;
+import com.micronixsolutions.api.music.model.MessagesArtistsResponse;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by juan on 9/18/13.
  */
-public class ArtistsGridFragment extends Fragment {
+public class ArtistsGridFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<MessagesArtistResponse>>{
+
+    ArtistGridAdapter mAdapter;
+    GridView mGridView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+ @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mAdapter = new ArtistGridAdapter(getActivity());
+        getLoaderManager().initLoader(0, null, this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView  = inflater.inflate(R.layout.artists_grid, container, false);
-        GridView gridView = (GridView) rootView.findViewById(R.id.artists_grid_view);
-        gridView.setAdapter(new ArtistGridAdapter(getActivity()));
+        mGridView = (GridView) rootView.findViewById(R.id.artists_grid_view);
+        mGridView.setAdapter(mAdapter);
         return rootView;
     }
 
     @Override
     public void onPause() {
         super.onPause();
+    }
+
+
+
+
+
+    // Loader Stuff
+    @Override
+    public Loader<List<MessagesArtistResponse>> onCreateLoader(int i, Bundle bundle) {
+        return new QueryArtistsLoader(getActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<MessagesArtistResponse>> loader, List<MessagesArtistResponse> data) {
+        mAdapter.setData(data);
+        mAdapter.notifyDataSetChanged();
+        mGridView.invalidateViews();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<MessagesArtistResponse>> loader) {
+        mAdapter.setData(null);
+    }
+
+
+
+
+
+
+    public static class QueryArtistsLoader extends AsyncTaskLoader<List<MessagesArtistResponse>>{
+        final Context mContext;
+        List<MessagesArtistResponse> mArtists;
+
+        public QueryArtistsLoader(Context context) {
+            super(context);
+            mContext = context;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            super.onStartLoading();
+            if(mArtists != null){
+                deliverResult(mArtists);
+            }
+            if(mArtists == null){
+                forceLoad();
+            }
+        }
+
+        @Override
+        public void stopLoading() {
+            super.stopLoading();
+            cancelLoad();
+        }
+
+        @Override
+        public void onCanceled(List<MessagesArtistResponse> data) {
+            super.onCanceled(data);
+        }
+
+        @Override
+        public List<MessagesArtistResponse> loadInBackground() {
+            Music.Builder builder = new Music.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
+            Music mApi = builder.build();
+            MessagesArtistsResponse artists = null;
+            try{
+                artists = mApi.music().artists().execute();
+            } catch (IOException e){
+                Log.d("ARTISTGRID", e.getMessage(), e);
+            }
+            return artists.getArtists();
+        }
+
+        @Override
+        public void deliverResult(List<MessagesArtistResponse> data) {
+            super.deliverResult(data);
+        }
     }
 }
